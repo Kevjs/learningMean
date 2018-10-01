a74b5729bbe371c3867ad22ecb30287a319ccafa
Kevin <kevjs@bu.edu>
2018-10-01 16:10:30 -0400

    adding all type of braces

--- PreMEAN/bracesValid.js
+++ PreMEAN/bracesValid.js
@@ -1,39 +1,66 @@
 // We are checking for matching curly braces, implementing the following
 // algorithm to solve, extra practice for parsing
-// expr = term term*
-// term = { term } | any other character than {} (including empty)
+// expr = term* 
+// term = { expr } | any other character than {} (including empty)
 
 var input;
 var index = 0;
 
-function expr(string){
+function checkBraces(string){
     input = string;
-    var result = true;
-    while(index < input.length && result){ result = term(); }
-    input = null;
     index = 0;
-    return result;
+    let answer = true;
+    while(index < input.length && answer) { answer = term(); };
+    return answer;
 }
 
-/*
-    If the index is over the length of the input return true to exit out of the function,
-    it will be set to false when it checks for closing brackets
-*/
 function term(){
-    if(index >= input.length){ return true; }
     if(input.charAt(index) === "{"){
         index++;
-        term();
+        let backtrack = index;
+        while(term2()){
+            backtrack = index;
+        }
+        index = backtrack;
         if(input.charAt(index) === "}"){
             index++;
             return true;
-        }return false;
-    }if(input.charAt(index) ==="}"){
-        return true;
+        }else{
+            return false;
+        }
+    }else if(input.charAt(index) === "["){
+        index++;
+        let backtrack = index;
+        while(term2()){
+            backtrack = index;
+        }
+        index = backtrack;
+        if(input.charAt(index) === "]"){
+            index++;
+            return true;
+        }else{
+            return false;
+        }
+    }else if(input.charAt(index) === "("){
+        index++;
+        let backtrack = index;
+        while(term2()){
+            backtrack = index;
+        }
+        index = backtrack;
+        if(input.charAt(index) === ")"){
+            index++;
+            return true;
+        }else{
+            return false;
+        }
     }
-    index++;
-    return term();
+    return false;
 }
 
-console.log("Positive result with random letters mixed: " +expr("a{b{ff}}{}{asdfasdfasdf}"));
-console.log("Negative result with random letters mixed: " +expr("a{b{}}{a{{asdf{}}"));
+console.log(checkBraces("{{}{}[]()}((({})))"));
+
+// console.log("Positive result with random letters mixed: " +expr("a{b{ff}}{}{asdfasdfasdf}"));
+// console.log("Negative result: " +expr("({[)}]"));
+// console.log("Positive result with random letters mixed: "+expr("({[{[{[(a)]}]}]}()){}"))
+// console.log("Negative result with random letters mixed: " +expr("a{b{}}{a{{asdf{}}"));
